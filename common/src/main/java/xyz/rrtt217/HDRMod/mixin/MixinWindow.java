847d944da93cf64d3c8d87b0c2c704c704410fa9
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-03-07 22:58:31 +0800

    fix: clean up after merge

--- common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinWindow.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinWindow.java
@@ -55,7 +55,7 @@ import static xyz.rrtt217.HDRMod.mixin.HDRModMixinPlugin.enableHDR;
             }
         }
         boolean applyWorkaround = (platform == GLFW.GLFW_PLATFORM_X11 || (hasNvidiaCard && platform == GLFW.GLFW_PLATFORM_WAYLAND) || (hasOnlyIntelCard && platform == GLFW.GLFW_PLATFORM_WIN32)) && !config.forceDisableGlfwWorkaround;
-        if(platform != GLFW.GLFW_PLATFORM_X11 && enableHDR && HDRMod.hasglfwLib) {
+        if(platform != GLFW.GLFW_PLATFORM_X11 && enableHDR && HDRModMixinPlugin.hasGlfwLib) {
             // For 16 bits per channal.
             GLFW.glfwWindowHint(GLFW.GLFW_RED_BITS, 16);
             GLFW.glfwWindowHint(GLFW.GLFW_GREEN_BITS, 16);
