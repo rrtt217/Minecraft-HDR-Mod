8be0921431f3cdc377548b9b238a8920bf111394
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-04-20 11:41:15 +0800

    feat: add proper setEnglishState for Linux

--- common/src/main/java/xyz/rrtt217/HDRMod/mixin/compat/imblocker/MixinIMManagerLinux.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/mixin/compat/imblocker/MixinIMManagerLinux.java
@@ -1,5 +1,6 @@
 package xyz.rrtt217.HDRMod.mixin.compat.imblocker;
 
+import io.github.reserveword.imblocker.common.LinuxIMFramework;
 import io.github.reserveword.imblocker.common.gui.Point;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.Minecraft;
@@ -21,9 +22,16 @@ import static xyz.rrtt217.HDRMod.util.ime.GLFWIMEUtils.glfwSetPreeditCursorRecta
 public class MixinIMManagerLinux {
     @Shadow
     private static boolean state;
+    @Shadow
+    private void checkIMFramework() {}
+    @Shadow
+    private LinuxIMFramework imFramework;
+
+
     // This only works on our modified GLFW, not the original LWJGL 3.4.1 version!
+    // Internally use zwp_text_input_v3::enable/disable or zwp_text_input_v1::activate/deactivate to completely disable IME when unneeded.
     @Inject(method = "setState", at = @At("HEAD"), cancellable = true, remap = false)
-    private void setState(boolean on, CallbackInfo ci) {
+    private void hdr_mod$LinuxSetState(boolean on, CallbackInfo ci) {
         HDRModConfig config = AutoConfig.getConfigHolder(HDRModConfig.class).getConfig();
         if(!config.enableIMBlockerSetStateIntegration) return;
         if (state != on) {
@@ -32,7 +40,17 @@ public class MixinIMManagerLinux {
         }
         ci.cancel();
     }
-    // This works on the original LWJGL 3.4.1 GLFW.
+
+    // Use IMBlocker original setState for setEnglighState. This will only run once when opening chat with / and won't affect much performance.
+    @Inject(method = "setEnglishState", at = @At("HEAD"), cancellable = true, remap = false)
+    private void hdr_mod$LinuxSetEnglishState(boolean englishState, CallbackInfo ci) {
+        this.checkIMFramework();
+        this.imFramework.setState(!englishState);
+        ci.cancel();
+    }
+
+
+    // This works on the original LWJGL 3.4.1 GLFW. Upstream accepted similar change in 26.1 and this will be unnecessary.
     @Unique
     public void updateCompositionWindowPos(Point pos) {
         HDRModConfig config = AutoConfig.getConfigHolder(HDRModConfig.class).getConfig();
