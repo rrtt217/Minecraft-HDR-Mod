8304305a73916c77df70abe6f5c5fe8725c6f809
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-06-30 15:05:24 +0800

    Now we can see title screen

--- common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinRenderPipelines.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinRenderPipelines.java
@@ -1,10 +1,19 @@
 package xyz.rrtt217.HDRMod.mixin;
 
 import com.mojang.blaze3d.GpuFormat;
+import com.mojang.blaze3d.PrimitiveTopology;
+import com.mojang.blaze3d.pipeline.ColorTargetState;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import net.minecraft.client.renderer.BindGroupLayouts;
 import net.minecraft.client.renderer.RenderPipelines;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Optional;
 
 @Mixin(RenderPipelines.class)
 public class MixinRenderPipelines {
@@ -14,5 +23,14 @@ public class MixinRenderPipelines {
             return GpuFormat.RGBA16_FLOAT;
         }
         return format;
+    }
+    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/pipeline/RenderPipeline$Builder;buildSnippet()Lcom/mojang/blaze3d/pipeline/RenderPipeline$Snippet;", ordinal = 23))
+    private static RenderPipeline.Snippet hdr_mod$modifyAnimationPipelineSnippet(RenderPipeline.Builder builder) {
+        return RenderPipeline.builder(RenderPipeline.builder().withBindGroupLayout(BindGroupLayouts.GLOBALS).buildSnippet())
+                .withVertexShader("core/animate_sprite")
+                .withBindGroupLayout(BindGroupLayouts.SPRITE_ANIMATION_INFO)
+                .withPrimitiveTopology(PrimitiveTopology.TRIANGLES)
+                .withColorTargetState(new ColorTargetState(Optional.empty(), GpuFormat.RGBA8_UNORM, 15))
+                .buildSnippet();
     }
 }
