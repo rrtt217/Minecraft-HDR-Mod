e3518df55846b7dc8c4722c88ca18caa2be608b1
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-07-04 22:50:18 +0800

    chore: forget that we can just copy instead of draw

--- common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinScreenshot.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinScreenshot.java
@@ -36,10 +36,6 @@ import java.util.function.Consumer;
 public class MixinScreenshot {
     @Unique
     private static GpuTexture hdr_mod$vanillaScreenshotFlipTexture;
-    @Unique
-    private static GpuTextureView hdr_mod$vanillaScreenshotFlipTextureView;
-    @Unique
-    private static final RenderPipeline hdr_mod$vanillaScreenshotConvertRenderPipeline = RenderPipeline.builder(new RenderPipeline.Snippet[0]).withBindGroupLayout(BindGroupLayouts.GLOBALS).withLocation("pipeline/screenshot_convert").withVertexShader("core/screenquad").withFragmentShader("core/blit_screen").withBindGroupLayout(BindGroupLayouts.IN_SAMPLER).withPrimitiveTopology(PrimitiveTopology.TRIANGLES).withColorTargetState(new ColorTargetState(Optional.empty(), GpuFormat.RGBA8_UNORM, 15)).build();;
     @Inject(method = "grab(Ljava/io/File;Ljava/lang/String;Lcom/mojang/blaze3d/pipeline/RenderTarget;ILjava/util/function/Consumer;)V", at = @At("HEAD"), cancellable = true)
     private static void onVanillaScreenshotCalled(File file, @Nullable String string, RenderTarget renderTarget, int i, Consumer<Component> consumer, CallbackInfo ci){
         HDRModConfig config = AutoConfig.getConfigHolder(HDRModConfig.class).getConfig();
@@ -62,29 +58,20 @@ public class MixinScreenshot {
     @WrapOperation(method = "takeScreenshot(Lcom/mojang/blaze3d/pipeline/RenderTarget;ILjava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/pipeline/RenderTarget;getColorTexture()Lcom/mojang/blaze3d/textures/GpuTexture;", ordinal = 0))
     private static GpuTexture hdr_mod$convertVanillaScreenshotFormat(RenderTarget instance, Operation<GpuTexture> original){
         if(hdr_mod$vanillaScreenshotFlipTexture != null){
-            hdr_mod$vanillaScreenshotFlipTextureView.close();
             hdr_mod$vanillaScreenshotFlipTexture.close();
         }
-
         hdr_mod$vanillaScreenshotFlipTexture = RenderSystem.getDevice().createTexture("vanilla screenshot format convert flip texture", GpuTexture.USAGE_COPY_DST | GpuTexture.USAGE_COPY_SRC | GpuTexture.USAGE_TEXTURE_BINDING | GpuTexture.USAGE_RENDER_ATTACHMENT, GpuFormat.RGBA8_UNORM, instance.width, instance.height, 1, 1);
-        hdr_mod$vanillaScreenshotFlipTextureView = RenderSystem.getDevice().createTextureView(hdr_mod$vanillaScreenshotFlipTexture);
-
-        try (RenderPass renderPass = RenderSystem.getDevice().createCommandEncoder().createRenderPass(() -> "Format Convert", hdr_mod$vanillaScreenshotFlipTextureView, Optional.empty())) {
-            RenderSystem.bindDefaultUniforms(renderPass);
-            renderPass.setPipeline(hdr_mod$vanillaScreenshotConvertRenderPipeline);
-            renderPass.bindTexture("InSampler", instance.getColorTextureView(), RenderSystem.getSamplerCache().getClampToEdge(FilterMode.NEAREST));
-            renderPass.draw(3, 1, 0, 0);
-            return hdr_mod$vanillaScreenshotFlipTexture;
+        if (instance.getColorTexture() != null) {
+            RenderSystem.getDevice().createCommandEncoder().copyTextureToTexture(instance.getColorTexture(), hdr_mod$vanillaScreenshotFlipTexture, 0, 0, 0, 0, 0, instance.width, instance.height);
         }
+        return hdr_mod$vanillaScreenshotFlipTexture;
     }
     @ModifyArg(method = "takeScreenshot(Lcom/mojang/blaze3d/pipeline/RenderTarget;ILjava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/CommandEncoder;copyTextureToBuffer(Lcom/mojang/blaze3d/textures/GpuTexture;Lcom/mojang/blaze3d/buffers/GpuBuffer;JLjava/lang/Runnable;I)V"), index = 3)
     private static Runnable hdr_mod$CloseTextureAfterScreenshot(Runnable callback){
         return () ->{
             callback.run();
-            hdr_mod$vanillaScreenshotFlipTextureView.close();
             hdr_mod$vanillaScreenshotFlipTexture.close();
             hdr_mod$vanillaScreenshotFlipTexture = null;
-            hdr_mod$vanillaScreenshotFlipTextureView = null;
         };
     }
 }
