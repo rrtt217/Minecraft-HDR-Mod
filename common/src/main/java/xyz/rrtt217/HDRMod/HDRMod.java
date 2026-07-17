a609a27a21cdb6d1c3e754c5e86cb36bd2b5055a
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-07-17 20:21:44 +0800

    feat: backport ColorManagementInfoProvider

--- common/src/main/java/xyz/rrtt217/HDRMod/HDRMod.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/HDRMod.java
@@ -19,6 +19,7 @@ import xyz.rrtt217.HDRMod.core.ColorTransformRenderer;
 import xyz.rrtt217.HDRMod.core.PngjHDRScreenshot;
 import org.slf4j.Logger;
 import xyz.rrtt217.HDRMod.config.HDRModConfig;
+import xyz.rrtt217.HDRMod.util.ColorManagementInfoProvider;
 
 import static xyz.rrtt217.HDRMod.compat.iris.IrisCompatibility.previousEnableHDR;
 
@@ -56,6 +57,8 @@ public final class HDRMod {
 
     public static ConfigHolder<HDRModConfig> configHolder;
 
+    public static ColorManagementInfoProvider colorManagementInfoProvider;
+
     public HDRMod() {
     }
 
@@ -90,6 +93,7 @@ public final class HDRMod {
         });
 
         HDRModConfig config = AutoConfig.getConfigHolder(HDRModConfig.class).getConfig();
+        colorManagementInfoProvider = new ColorManagementInfoProvider(config);
         previousEnableHDR = config.enableHDR;
         LOGGER.debug("HDRMod Initialized!");
     }
