1935c8d9a26bd1cef52191ac180b5cf71a87141b
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-08-26 00:20:57 +0800

    fix: quick fix for BlazeSDL redirect

--- common/src/main/java/xyz/rrtt217/HDRMod/mixin/compat/blazesdl/MixinSDLGlSurface.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/mixin/compat/blazesdl/MixinSDLGlSurface.java
@@ -1,5 +1,7 @@
 package xyz.rrtt217.HDRMod.mixin.compat.blazesdl;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.lwjgl.sdl.SDLVideo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,10 +18,10 @@ public class MixinSDLGlSurface {
         if(HDRMod.glInteropResourceManager.presentSwapchain()) ci.cancel();
     }
 
-    @Redirect(method = "configure", at = @At(value = "INVOKE", target = "Lorg/lwjgl/sdl/SDLVideo;SDL_GL_SetSwapInterval(I)Z"))
-    private boolean configure(int interval) {
+    @WrapOperation(method = "configure", at = @At(value = "INVOKE", target = "Lorg/lwjgl/sdl/SDLVideo;SDL_GL_SetSwapInterval(I)Z"))
+    private boolean configure(int interval, Operation<Boolean> original) {
         if(!HDRMod.glInteropResourceManager.setSwapInterval(interval))
-            SDLVideo.SDL_GL_SetSwapInterval(interval);
+            return original.call(interval);
         return true;
     }
 }
